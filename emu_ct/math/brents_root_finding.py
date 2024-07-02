15123bc0c35ebf22f1db37f59988d9bd8970d85c
Pablo Le Henaff <pablo.le-henaff@pasqal.com>
2024-07-02 18:07:55 +0200

    Add Brent's root finding algorithm with tests

---
+++ emu_ct/math/brents_root_finding.py
@@ -0,0 +1,84 @@
+from typing import Callable
+
+"""
+Finds the zero of a scalar function using Brent's method.
+"""
+
+
+def find_root_brents(
+    f: Callable[[float], float],
+    a: float,
+    b: float,
+    fa: float | None = None,
+    fb: float | None = None,
+    /,
+    *,
+    tolerance: float = 1e-6,
+    epsilon: float = 1e-6,
+) -> float:
+    fa = fa or f(a)
+    fb = fb or f(b)
+
+    assert fa * fb < 0, "Function root needs to be between a and b"
+
+    # b has to be the better guess
+    if abs(fa) < abs(fb):
+        a, b = b, a
+        fa, fb = fb, fa
+
+    c = a
+    d = c
+    fc = fa
+
+    bisection = True
+    while abs(b - a) > tolerance:
+        if abs(fc - fa) < epsilon or abs(fc - fb) < epsilon:
+            # Secant method
+            dx = fb * (b - a) / (fa - fb)
+        else:
+            # Inverse quadratic interpolation
+            s = fb / fa
+            r = fb / fc
+            t = fa / fc
+            q = (t - 1) * (s - 1) * (r - 1)
+            p = s * (t * (r - t) * (c - b) + (r - 1) * (b - a))
+            dx = p / q
+
+        # Use bisection instead of interpolation
+        # if the interpolation is not within bounds.
+        delta = abs(2 * epsilon * b)
+        adx = abs(dx)
+        delta_bc = abs(b - c)
+        delta_cd = abs(c - d)
+        delta_ab = a - b
+        if (
+            (adx >= abs(3 * delta_ab / 4) or dx * delta_ab < 0)
+            or (bisection and adx >= delta_bc / 2)
+            or (not bisection and adx >= delta_cd / 2)
+            or (bisection and delta_bc < delta)
+            or (not bisection and delta_cd < delta)
+        ):
+            dx = (a - b) / 2
+            bisection = True
+        else:
+            bisection = False
+
+        x = b + dx
+        fx = f(x)
+        d = c
+        c, fc = b, fb
+
+        # Update interval
+        if fa * fx < 0:
+            b, fb = x, fx
+            fb = fx
+        else:
+            a, fa = x, fx
+            fa = fx
+
+        # b has to be the better guess
+        if abs(fa) < abs(fb):
+            a, b = b, a
+            fa, fb = fb, fa
+
+    return b
---
+++ tests/math/test_brents_root_finding.py
@@ -0,0 +1,138 @@
+from emu_ct.math.brents_root_finding import find_root_brents
+import random
+from unittest.mock import MagicMock
+import torch
+
+test_tolerance = 1e-10
+
+"""
+Returns the result of `find_root_brents` but also the number of times `f` was called.
+"""
+
+
+def find_root_brents_instrumented(
+    f,
+    a,
+    b,
+    fa: float | None = None,
+    fb: float | None = None,
+    *,
+    tolerance: float = 1e-6,
+    epsilon: float = 1e-6,
+) -> (float, int):
+    mock_f = MagicMock()
+    mock_f.side_effect = f
+
+    result_root = find_root_brents(
+        mock_f, a, b, fa, fb, tolerance=tolerance, epsilon=epsilon
+    )
+    result_call_count = mock_f.call_count
+
+    return result_root, result_call_count
+
+
+def test_find_root_polynomial():
+    P = lambda x: (x + 3) * (x - 1) ** 2
+
+    actual_root, call_count = find_root_brents_instrumented(P, -4, 4 / 3.0)
+
+    expected_root = -3
+
+    assert abs(actual_root - expected_root) < test_tolerance
+    assert call_count == 12
+
+
+def test_find_root_segments():
+    def f(x: float) -> float:
+        if x < 0:
+            return -1
+        return -1 + x * 10
+
+    actual_root, call_count = find_root_brents_instrumented(f, -10, 10)
+
+    expected_root = 0.1
+
+    assert abs(actual_root - expected_root) < test_tolerance
+    assert call_count == 13
+
+
+def test_find_root_integral():
+    # Create a random continuous monotonously increasing function
+    # by integrating a random >= 0 stairs function.
+
+    random.seed(0xDEADBEEF)
+    n = 100  # Number of stair steps
+    diff_values = [random.random() for _ in range(n)]  # Steps heights
+    slice_size = 1 / n  # Width of stair step
+
+    m = (
+        sum(diff_values) * slice_size
+    )  # Integral of the whole stair function between 0 and 1
+    init = -m / 2
+
+    def f(x: float) -> float:
+        # Integral between 0 and x of the stairs.
+
+        assert 0 <= x <= 1
+
+        if x >= 1:
+            return m
+
+        index = int(x * n)
+
+        return (
+            init
+            + sum(diff_values[:index]) * slice_size
+            + x % slice_size * diff_values[index]
+        )
+
+    # Compute the expected root.
+    acc = init
+    i = 0
+    while acc + slice_size * diff_values[i] <= 0:
+        acc += slice_size * diff_values[i]
+        i += 1
+
+    expected_root = i * slice_size - acc / diff_values[i]
+
+    actual_root, call_count = find_root_brents_instrumented(f, 0, 1)
+
+    assert abs(actual_root - expected_root) < test_tolerance
+    assert call_count == 18
+
+
+def test_find_root_store_intermediate_result():
+    # For noisy mcwf time steps, we need the scalar function
+    # to store intermediate results: the evolved state.
+    # This test gives and example of this and makes sure that the last function evaluation
+    # corresponds to the found root.
+    n = 10
+
+    torch.manual_seed(0xDEADBEEF)
+    lindblad = torch.rand(n, n, dtype=torch.complex128)
+    rate = 0.1
+    hami = torch.rand(n, n, dtype=torch.complex128)
+    hami += hami.T.conj()
+    noisy_hami = hami - 1j / 2.0 * rate * lindblad.T.conj() @ lindblad
+    psi = torch.rand(n, dtype=torch.complex128)
+    psi /= psi.norm()
+
+    target_norm = 0.5
+
+    psi_evolved = None
+
+    def f(t: float) -> float:
+        nonlocal psi_evolved
+        psi_evolved = torch.matmul(torch.linalg.matrix_exp(-1j * t * noisy_hami), psi)
+        assert (
+            psi_evolved.norm() <= psi.norm()
+        ), "Norm of the state should decrease with time"
+
+        return psi_evolved.norm() - target_norm
+
+    found_root, call_count = find_root_brents_instrumented(f, 0, 20)
+
+    assert abs(f(found_root)) < test_tolerance
+    assert (psi_evolved.norm() - target_norm) < test_tolerance
+
+    assert call_count == 12
