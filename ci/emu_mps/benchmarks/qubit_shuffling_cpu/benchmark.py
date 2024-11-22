f2c63fa1c22f763c3c3c97cd44ac99386a6e64e8
Kemal Bidzhiev <kemal.bidzhiev@pasqal.com>
2024-11-22 09:32:17 +0000

    Kb/rss

--- ci/emu_mps/benchmarks/qubit_shuffling_cpu/benchmark.py
+++ ci/emu_mps/benchmarks/qubit_shuffling_cpu/benchmark.py
@@ -3,26 +3,24 @@ import emu_mps
 import random
 from benchmarkutils.sequenceutils import make_adiabatic_afm_state_2d_seq
 from benchmarkutils.plotutils import plot_observables_and_performance
+import multiprocessing as mp
 
 script_dir = Path(__file__).parent
 res_dir = script_dir / "results"
 res_dir.mkdir(exist_ok=True)
 
-title = "Qubit shuffling 2d - CPU"
-print(f"Starting {title} benchmark")
+statistics = mp.Manager().dict()
 
 Nx = 4
 Ny = 4
 
-random.seed(2024)
-perm_maps = {
-    "unshuffled": range(Nx * Ny),
-    "shuffled": random.sample(range(Nx * Ny), k=Nx * Ny),
-}
+title = f"Qubit shuffling {Nx}x{Ny} 2d - CPU"
+print(f"Starting {title} benchmark")
 
-all_results = {}
 
-for test_name, perm_map in perm_maps.items():
+def run_simulation(Nx, Ny, perm_map, test_name):
+    backend = emu_mps.MPSBackend()
+
     seq = make_adiabatic_afm_state_2d_seq(Nx, Ny, perm_map=perm_map)
 
     dt = 10
@@ -40,12 +38,24 @@ for test_name, perm_map in perm_maps.items():
     config = emu_mps.MPSConfig(
         num_gpus_to_use=0, observables=obs, log_file=res_dir / f"log_{test_name}.log"
     )
-    backend = emu_mps.MPSBackend()
-
     results = backend.run(seq, mps_config=config)
+
     results.dump(res_dir / f"results_{test_name}.json")
-    all_results[test_name] = results
+    statistics[test_name] = results
+
+
+random.seed(2024)
+perm_maps = {
+    "unshuffled": range(Nx * Ny),
+    "shuffled": random.sample(range(Nx * Ny), k=Nx * Ny),
+}
+
+for test_name, perm_map in perm_maps.items():
+    # Run simulation in a separate process for correct measurement of ru_maxrss.
+    process = mp.Process(target=run_simulation, args=(Nx, Ny, perm_map, test_name))
+    process.start()
+    process.join()
 
 plot_observables_and_performance(
-    all_results=all_results, title=title, output_dir=res_dir, perm_maps=perm_maps
+    all_results=statistics, title=title, output_dir=res_dir, perm_maps=perm_maps
 )
