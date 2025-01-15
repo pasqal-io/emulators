cb7f85d3920e5ba9e82a5790ac40f75f2b2b23d4
MauroMendizabal <mauro.mendizabal-pico@pasqal.com>
2025-01-15 17:00:26 +0100

    magicMock for DenseOperator and QubitDensity

--- test/emu_sv/test_custom_callbacks.py
+++ test/emu_sv/test_custom_callbacks.py
@@ -6,8 +6,11 @@ from emu_base.base_classes.default_callbacks import QubitDensity
 
 from pytest import approx
 
+from unittest.mock import MagicMock
+
 
 def test_custom_qubit_density():
+    #set up for state
     basis = ("r", "g")
     num_qubits = 4
     strings = {"rrrr": 1.0, "ggggg": 1.0}
@@ -16,9 +19,17 @@ def test_custom_qubit_density():
     )
 
     config = SVConfig()
-    H = DenseOperator
+    
+    MockOperator = MagicMock(spec=DenseOperator)
+
+    H_mock = MockOperator.return_value
+
+    MockQubitDensity = MagicMock(spec=QubitDensity)
+
+    qubitDenistyMock = MockQubitDensity.return_value 
+
     t = 1
 
-    qubit_density = custom_qubit_density(QubitDensity, config, t, state, H)
+    qubit_density = custom_qubit_density(qubitDenistyMock, config, t, state, H_mock)
     expected = [0.5] * num_qubits
     assert qubit_density == approx(expected, abs=1e-8)
