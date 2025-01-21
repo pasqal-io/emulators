1ac1705349b6dc842e573a36eaf479b14be4df28
MauroMendizabal <mauro.mendizabal-pico@pasqal.com>
2025-01-21 12:02:16 +0100

    qubit density test not passing

--- test/emu_sv/test_end_to_end.py
+++ test/emu_sv/test_end_to_end.py
@@ -156,7 +156,7 @@ def test_end_to_end_afm_ring():
     assert approx(energy, 1e-8) == -115.34554274708604
 
     energy_variance = result["energy_variance"][final_time]  # 45.911110563993134
-    assert approx(energy_variance, 1e-8) == 45.91111056399
+    assert approx(energy_variance) == 45.91111056399
 
     second_moment_energy = result["second_moment_of_energy"][
         final_time
