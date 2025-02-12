1d5ea839c1d8dda66be53203fc2b5eb4ec9810ff
Kemal Bidzhiev <kemal@Kemals-MacBook-Pro.local>
2025-02-12 16:21:20 +0100

    better naming

--- test/base_classes/test_config.py
+++ test/base_classes/test_config.py
@@ -4,26 +4,28 @@ import torch
 import numpy
 
 
-@pytest.mark.parametrize(
-    "n_qubits",
-    [10, 25, 50],
-)
-def test_interaction_matrix(n_qubits: int) -> None:
+def test_interaction_matrix_type() -> None:
     BackendConfig(interaction_matrix=None)  # None is ok, empty tensor not ok
-    BackendConfig(interaction_matrix=[[0.0, 1.0], [1.0, 0.0]])
 
-    def test_BackendConfig(int_mat) -> None:
+    def test_input_type(int_mat) -> None:
         with pytest.raises(
             ValueError,
             match="Interaction matrix must be provided as a Python list of lists of floats",
         ):
             BackendConfig(interaction_matrix=int_mat)
 
-    test_BackendConfig(int_mat=[1, 2, 3])
-    test_BackendConfig(int_mat=torch.eye(3))
-    test_BackendConfig(int_mat=numpy.eye(3))
+    test_input_type(int_mat=[[0, 1], [1, 0]])  # not float
+    test_input_type(int_mat=[0, 1, 2, 3, 4])  # not list of lists
+    test_input_type(int_mat=torch.eye(5))  # not list of lists
+    test_input_type(int_mat=numpy.eye(5))  # not list of lists
 
-    def test_BackendConfig(int_mat: torch.tensor) -> None:
+
+@pytest.mark.parametrize(
+    "n_qubits",
+    [10, 15, 20],
+)
+def test_interaction_matrix_properties(n_qubits: int) -> None:
+    def test_symmetric_zero_diag(int_mat: torch.tensor) -> None:
         with pytest.raises(
             ValueError, match="Interaction matrix is not symmetric and zero diag"
         ):
@@ -32,25 +34,24 @@ def test_interaction_matrix(n_qubits: int) -> None:
     wrong_interaction_matrix = torch.randn(
         n_qubits, n_qubits + 1, dtype=torch.float64
     )  # not square
-    test_BackendConfig(wrong_interaction_matrix)
+    test_symmetric_zero_diag(wrong_interaction_matrix)
 
     wrong_interaction_matrix = torch.randn(
         n_qubits, n_qubits - 1, dtype=torch.float64
     )  # not square
-    test_BackendConfig(wrong_interaction_matrix)
+    test_symmetric_zero_diag(wrong_interaction_matrix)
 
     wrong_interaction_matrix = torch.randn(
         n_qubits, n_qubits, dtype=torch.float64
     )  # not symmetric
-    test_BackendConfig(wrong_interaction_matrix)
+    test_symmetric_zero_diag(wrong_interaction_matrix)
 
     wrong_interaction_matrix = (
         wrong_interaction_matrix + wrong_interaction_matrix.T
     )  # diagonal not 0
-    test_BackendConfig(wrong_interaction_matrix)
+    test_symmetric_zero_diag(wrong_interaction_matrix)
 
-    correct_interaction_matrix = wrong_interaction_matrix.fill_diagonal_(0)
-    correct_interaction_matrix = correct_interaction_matrix.tolist()
+    correct_interaction_matrix = wrong_interaction_matrix.fill_diagonal_(0).tolist()
     BackendConfig(
         interaction_matrix=correct_interaction_matrix,
     )
