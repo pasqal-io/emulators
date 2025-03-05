e21fbcaef69811b2bf812cc457e9a2ab4fec0381
stefano.grava <stefano.grava@pasqal.com>
2025-03-05 10:26:58 +0100

    double Krylov and test

---
+++ emu_base/math/double_krylov.py
@@ -0,0 +1,99 @@
+import torch
+from typing import Callable
+
+
+from emu_base.math.krylov_exp import DEFAULT_MAX_KRYLOV_DIM
+
+# some number larger than needed
+max_krylov_dim = DEFAULT_MAX_KRYLOV_DIM
+
+
+def double_krylov(
+    op: Callable,
+    grad: torch.Tensor,
+    state: torch.Tensor,
+    iteration_count: int,
+    tolerance: float,
+) -> tuple[list, torch.Tensor, torch.Tensor]:
+    """
+            |op |state><grad||
+    let h = |0        op     |
+    Then this computes
+    V = Gram-Scmidt(grad,state,op(grad),op(state),op^2(grad),op^2(state),...)
+    and e^T = V^-1 exp(h) V
+    """
+
+    lanczos_vectors_even = [grad / grad.norm()]
+    lanczos_vectors_odd = torch.zeros(
+        iteration_count + 1, state.shape[0], dtype=torch.complex128, device=state.device
+    )
+    lanczos_vectors_odd[0] = state / state.norm()
+    Tb = torch.zeros(2 * max_krylov_dim + 4, 2 * max_krylov_dim + 4, dtype=state.dtype)
+    Tb[1, 0] = (
+        grad.norm() * state.norm()
+    )  # the A matrix in the top left overlaps these two vectors, and nothing else
+    T = Tb[1::2, 1::2]
+    for j_o in range(iteration_count):
+        w = op(lanczos_vectors_odd[j_o])
+
+        for k in range(max(0, j_o - 1), j_o + 1):
+            tmp = torch.tensordot(lanczos_vectors_odd[k].conj(), w, dims=w.dim())
+            T[k, j_o] = tmp
+            w -= tmp * lanczos_vectors_odd[k]
+
+        tmp = w.norm()
+        T[j_o + 1, j_o] = tmp
+
+        if tmp.real < tolerance:
+            # Happy breakdown
+            lanczos_vectors_odd = lanczos_vectors_odd[:-1]
+            break
+
+        lanczos_vectors_odd[j_o + 1] = w / tmp
+
+        # Compute exponential of extended T matrix
+        T[j_o + 2, j_o + 1] = 1
+    if T[j_o + 2, j_o + 1]:
+        size_odd = j_o + 3
+    else:
+        size_odd = j_o + 1
+    current_vector = 0
+    old = 1e12
+    new = 1e12
+    n = 1e12
+    for j_e in range(max_krylov_dim):  # TODO: exit condition
+        w = op(lanczos_vectors_even[-1])
+        for k in range(max(0, j_e - 1), j_e + 1):
+            # overlaps of the even subspace
+            Tb[2 * k, 2 * j_e] = torch.tensordot(
+                lanczos_vectors_even[k].conj(), w, dims=w.dim()
+            )
+            w -= Tb[2 * k, 2 * j_e] * lanczos_vectors_even[k]
+        # even
+        tmp = w.norm()
+        Tb[2 * j_e + 2, 2 * j_e] = tmp
+        if tmp.real < tolerance:
+            # Happy breakdown
+            size = max(2 * j_e + 2, 2 * size_odd)
+            expd = torch.linalg.matrix_exp(Tb[:size, :size])
+            current_vector = j_e
+            break
+        lanczos_vectors_even.append(w / tmp)
+        # Compute exponential of extended T matrix
+        Tb[2 * j_e + 4, 2 * j_e + 2] = 1
+        size = max(2 * j_e + 6, 2 * size_odd)
+        expd = torch.linalg.matrix_exp(Tb[:size, :size])
+        old = new
+        new = abs(expd[1, 2 * current_vector])
+
+        err = abs(old - new)
+
+        if err < tolerance:
+            # check if we have enough vectors for the trace
+            tr_err = expd[1:size:2, 2 * current_vector]
+            n = tr_err.norm()
+            if n < tolerance:
+                break
+            current_vector += 1
+            new = abs(expd[1, 2 * current_vector])
+    return lanczos_vectors_even[: current_vector + 1], lanczos_vectors_odd, expd
---
+++ test/emu_base/math/test_double_krylov.py
@@ -0,0 +1,58 @@
+import torch
+from emu_base.math.double_krylov import double_krylov
+from emu_sv.hamiltonian import RydbergHamiltonian
+from test.utils_testing.utils_dense_hamiltonians import dense_rydberg_hamiltonian
+from pytest import mark
+
+dtype = torch.complex128
+dtype_params = torch.float64
+
+
+@mark.parametrize(
+    "N, tolerance",
+    [(n, tol) for n in [5, 7] for tol in [1e-8, 1e-10, 1e-12]],
+)
+def test_double_krylov(N, tolerance):
+    torch.manual_seed(1337)
+    omegas = torch.randn(N, dtype=dtype_params)
+    deltas = torch.randn(N, dtype=dtype_params)
+    phis = torch.randn(N, dtype=dtype_params)
+    interactions = torch.zeros(N, N, dtype=dtype_params)
+    for i in range(N):
+        for j in range(i + 1, N):
+            interactions[i, j] = 1 / abs(j - i)
+    state = torch.randn(2**N, dtype=dtype)
+    state = state / state.norm()
+
+    grad = torch.randn(2**N, dtype=dtype)
+    #grad = grad / grad.norm()
+
+    dt, iteration_count = 1.0, 80
+    tolerance = 1e-8
+    ham = RydbergHamiltonian(
+        omegas=omegas,
+        deltas=deltas,
+        phis=phis,
+        interaction_matrix=interactions,
+        device=state.device,
+    )
+
+    op = lambda x: -1j * dt * (ham * x)
+    lanczos_vectors_even, odd_block, eT = double_krylov(
+        op, grad, state, iteration_count, tolerance
+    )
+
+    even_block = torch.stack(lanczos_vectors_even)
+    Hess_L = eT[1 : 2 * odd_block.shape[0] : 2, : 2 * even_block.shape[0] : 2]
+    # L = V_odd @ Hess_L @ V_even*
+    L = odd_block.mT @ Hess_L @ even_block.conj()
+
+    Hsv = dense_rydberg_hamiltonian(omegas, deltas, phis, interactions)
+    E = state.unsqueeze(-1) @ grad.conj().unsqueeze(0)
+    big_mat = torch.block_diag(-1j * dt * Hsv, -1j * dt * Hsv)
+    sizeH = Hsv.shape[0]
+    big_mat[:sizeH, sizeH:] = E
+    big_exp = torch.linalg.matrix_exp(big_mat)
+    expected_L = big_exp[:sizeH, sizeH:]
+
+    assert torch.allclose(L, expected_L, atol=tolerance)
