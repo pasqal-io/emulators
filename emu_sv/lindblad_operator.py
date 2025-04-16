e8a1b37465495dfbd02e2cdd6336e9ca900250bd
murogrande <murogrande@yahoo.com>
2025-04-16 15:27:41 +0200

    save point

--- emu_sv/lindblad_operator.py
+++ emu_sv/lindblad_operator.py
@@ -10,7 +10,7 @@ class LindbladOperator:
         omegas: torch.Tensor,
         deltas: torch.Tensor,
         phis: torch.Tensor,
-        pulser_linblad: torch.Tensor,
+        pulser_linblads: list[torch.Tensor],
         interaction_matrix: torch.Tensor,
         device: torch.device,
     ):
@@ -19,6 +19,7 @@ class LindbladOperator:
         self.deltas: torch.Tensor = deltas
         self.phis: torch.Tensor = phis
         self.interaction_matrix: torch.Tensor = interaction_matrix
+        self.pulser_linblads: list[torch.Tensor] = pulser_linblads
         self.device: torch.device = device
 
         self.diag: torch.Tensor = self._create_diagonal()
@@ -43,32 +44,33 @@ class LindbladOperator:
         return diag.view(-1)
 
     def apply_sum_sigma_x_i_to_density(self, rho: torch.Tensor) -> torch.Tensor:
-        # Convert rho to a tensor with shape (2,)* (2*nqubits),
+        # convert rho to a tensor with shape (2,)* (2*nqubits),
         # where the first nqubits dimensions are for the row indices.
         full_shape = (2,) * (2 * self.nqubits)
         rho_view = rho.view(full_shape)
 
-        # Create an accumulator tensor with the same shape and data type as rho_view.
+        # create an accumulator tensor with the same shape and data type as rho_view.
         accum = torch.zeros_like(rho_view)
 
-        # Loop over each qubit j and apply the Pauli X  along the row index (dimension j)
+        # loop over each qubit j and apply the Pauli X  along the row index j
         for j, omega_j in enumerate(self.omegas):
-            # torch.flip reverses the order along dimension j,  swaps 0 and 1.
+            # flip reverses the order along dimension j,  swaps 0 and 1.
             h_rho_term = torch.flip(rho_view, dims=[j])
-            # Accumulate the weighted contribution of this term in-place
-            accum.add_(omega_j * h_rho_term)
+            # Accumulate the contribution
+            accum.add_(omega_j * h_rho_term)  # in place opperation
 
-        # Return the resulting tensor in its original 2^n x 2^n matrix shape.
+        # return the resulting tensor in its original 2^n x 2^n matrix shape.
         return accum.view(2**self.nqubits, 2**self.nqubits)
 
     def apply_local_operator_to_density_matrix(
         self, density_matrix: torch.Tensor, local_op: torch.Tensor, target_qubit: int
     ) -> torch.Tensor:
-        # Reshape density matrix to a 2n-way tensor of shape (2,...,2) x (2,...,2)
+        # reshape density matrix to a 2n-way tensor of shape (2,...,2) x (2,...,2)
         rho = density_matrix.view([2] * (2 * self.nqubits))
 
-        # Permute so that the target qubit comes first in both bra and ket spaces.
-        # Determine new ordering for the bra (first n_qubits) and for ket (next n_qubits) indices.
+        # permute so that the target qubit comes first in both bra and ket spaces.
+        # determine new ordering for the bra (first n_qubits) and
+        # for ket (next n_qubits) indices.
         perm = list(range(self.nqubits))
         perm.remove(target_qubit)
         perm = [target_qubit] + perm  # target qubit now in the first position
@@ -77,32 +79,34 @@ class LindbladOperator:
         total_perm = bra_perm + ket_perm
         rho = rho.permute(total_perm)
 
-        # Reshape to a 2-index tensor: (2, 2^(n-1)* 2* 2^(n-1)) in order to multiply with A.
-        # The first index is the target qubit, the second index is the rest of the qubits.
+        # reshape to a 2-index tensor: (2, 2^(n-1)* 2* 2^(n-1)) in order to multiply with local_op.
+        # the first index is the target qubit, the second index is the rest of the qubits.
         rho = rho.contiguous().view(2, 2 ** (2 * self.nqubits - 1))
 
-        # Apply A to the bra index.
-        # Contract A (indices 'ab') with the first index of rho ('b') giving new index 'a'.
+        # apply local_op to the bra index.
+        # contract local_op (indices 'ab') with the first index of rho ('b') giving new index 'a'.
         # rho = torch.einsum("ab,bijc->aijc", local_op, rho)
         rho = local_op @ rho
 
-        # Reshape back to a 2n-way tensor.
+        # reshape back to a 2n-way tensor.
         rho = rho.contiguous().view([2] * (2 * self.nqubits))
 
-        # Invert the permutation to return the indices to their original order.
+        # invert the permutation to return the indices to their original order.
         inv_perm = [0] * (2 * self.nqubits)
         for i, p in enumerate(total_perm):
             inv_perm[p] = i
         rho = rho.permute(inv_perm)
 
-        # Reshape back to the full density matrix shape [2**n, 2**n].
+        # reshape back to the full density matrix shape [2**n, 2**n].
         return rho.contiguous().view(2**self.nqubits, 2**self.nqubits)
 
     def __matmul__(self, densi_matrix: torch.Tensor) -> torch.Tensor:
+        # Constructing Hₑ = H 𝜌 −0.5∑ₖ L^† L 𝜌
 
+        # Applying ∑ᵢ 𝛺 /2 𝜎ₓ terms in H
         densi_matrix_sum_x = self.apply_sum_sigma_x_i_to_density(densi_matrix)
 
-        # (-∑ⱼΔⱼnⱼ + ∑ᵢ﹥ⱼUᵢⱼnᵢnⱼ)|ψ❭
+        # -∑ⱼΔⱼnⱼ + ∑ᵢ﹥ⱼUᵢⱼnᵢnⱼ in H
         storage = torch.zeros_like(densi_matrix, dtype=dtype)
         shape = densi_matrix.shape[0]
         for i in range(shape):
@@ -110,4 +114,12 @@ class LindbladOperator:
             storage[:, i] = diag_result
         # Add the diagonal result to the densi_matrix
         densi_matrix = densi_matrix_sum_x + storage
+
+        # Applying the Lindblad operators
+        for i, pulser_linblad in enumerate(self.pulser_linblads):
+            storage_lindblads = self.apply_local_operator_to_density_matrix(
+                densi_matrix, pulser_linblad.conj().T @ pulser_linblad, i
+            )
+            densi_matrix -= 0.5 * storage_lindblads
+
         return densi_matrix
--- test/emu_sv/test_linblad_operator.py
+++ test/emu_sv/test_linblad_operator.py
@@ -1,3 +1,5 @@
+import math
+import pytest
 import torch
 from emu_sv.lindblad_operator import LindbladOperator
 from test.utils_testing import dense_rydberg_hamiltonian, nn_interaction_matrix
@@ -13,7 +15,7 @@ def test_ham_matmul_density():
     omegas = torch.rand(nqubits, dtype=dtype, device=device)
     deltas = torch.rand(nqubits, dtype=dtype, device=device)
     phis = torch.zeros(nqubits, dtype=dtype, device=device)
-    pulser_linblad = torch.zeros(2**nqubits, dtype=dtype, device=device)
+    pulser_linblad = [torch.zeros(2, 2, dtype=dtype, device=device)]
     interaction_matrix = nn_interaction_matrix(nqubits)
 
     rho = torch.rand(2**nqubits, 2**nqubits, dtype=dtype, device=device)
@@ -21,7 +23,7 @@ def test_ham_matmul_density():
         omegas=omegas,
         deltas=deltas,
         phis=phis,
-        pulser_linblad=pulser_linblad,
+        pulser_linblads=pulser_linblad,
         interaction_matrix=interaction_matrix,
         device=device,
     )
@@ -32,6 +34,80 @@ def test_ham_matmul_density():
     )
     h_rho = ham @ rho
 
-    assert torch.allclose(
-        result, h_rho, atol=1e-5
-    ), "LindbladOperator with no Lindblad test failed"
+    assert torch.allclose(result, h_rho)
+
+
+@pytest.mark.parametrize(
+    "target_qubit",
+    [0, 1, 2, 3, 4, 5],
+)
+def test_apply_local_operator_on_target_qubit(target_qubit):
+    nqubits = 6  # Total number of qubits
+    target = target_qubit  # Qubit on which A acts
+
+    omegas = torch.zeros(nqubits, dtype=dtype, device=device)
+    deltas = torch.zeros(nqubits, dtype=dtype, device=device)
+    phis = torch.zeros(nqubits, dtype=dtype, device=device)
+    pulser_linblad = torch.zeros(2**nqubits, dtype=dtype, device=device)
+    interaction_matrix = torch.zeros(nqubits, nqubits)
+
+    ham_lind = LindbladOperator(
+        omegas=omegas,
+        deltas=deltas,
+        phis=phis,
+        pulser_linblads=pulser_linblad,
+        interaction_matrix=interaction_matrix,
+        device=device,
+    )
+
+    # Define a random 2x2 complex matrix
+    lindblad_op = torch.randn(2, 2, dtype=dtype, device=device)
+    rho = torch.randn(2**nqubits, 2**nqubits, dtype=dtype, device=device)
+
+    # Apply the local operator
+    updated_rho = ham_lind.apply_local_operator_to_density_matrix(
+        density_matrix=rho, local_op=lindblad_op, target_qubit=target
+    )
+
+    ident = torch.eye(2, dtype=dtype, device=device)
+
+    lista = []
+    for i in range(nqubits):
+        if i == target:
+            lista.append(lindblad_op)
+        else:
+            lista.append(ident)
+
+    res = torch.kron(lista[0], lista[1])
+    for i in range(2, nqubits):
+        res = torch.kron(res, lista[i])
+
+    assert torch.allclose(updated_rho, res @ rho)
+
+
+def test_lindblads():
+    torch.manual_seed(234)
+    nqubits = 2
+    omegas = torch.zeros(nqubits, dtype=dtype, device=device)
+    deltas = torch.zeros(nqubits, dtype=dtype, device=device)
+    phis = torch.zeros(nqubits, dtype=dtype, device=device)
+    pulser_linblad = [
+        math.sqrt(1 / 2)
+        * torch.tensor([[0.0, 1.0], [0.0, 0.0]], dtype=dtype, device=device)
+    ]
+    interaction_matrix = torch.zeros(
+        nqubits, nqubits, dtype=dtype, device=device
+    )  # nn_interaction_matrix(nqubits)
+
+    rho = torch.rand(2**nqubits, 2**nqubits, dtype=dtype, device=device)
+    ham_lind = LindbladOperator(
+        omegas=omegas,
+        deltas=deltas,
+        phis=phis,
+        pulser_linblads=pulser_linblad,
+        interaction_matrix=interaction_matrix,
+        device=device,
+    )
+    result = ham_lind @ rho
+
+    print(result)
