512ba788e683496067c5e788d0b00e5047a96a6f
Mauro Mendizábal <murogrande@yahoo.com>
2025-05-05 11:57:59 +0200

    Update emu_base/jump_lindblad_operators.py
    stefano's comment
    
    Co-authored-by: Stefano Grava <42062939+sgrava@users.noreply.github.com>

--- emu_base/jump_lindblad_operators.py
+++ emu_base/jump_lindblad_operators.py
@@ -56,11 +56,5 @@ def compute_noise_from_lindbladians(lindbladians: list[torch.Tensor]) -> torch.T
         lindbladian.shape == (2, 2) for lindbladian in lindbladians
     ), "Only single-qubit lindblad operators are supported"
 
-    return (
-        -1j
-        / 2.0
-        * sum(
-            (lindbladian.T.conj() @ lindbladian for lindbladian in lindbladians),
-            start=torch.zeros(2, 2, dtype=torch.complex128),
-        )
+    return -0.5j*sum(L.mH @ L for L in lindbladians)
     )
