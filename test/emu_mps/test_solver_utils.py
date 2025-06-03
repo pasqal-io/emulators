67e762c82c7a724e1296bd785ef9faad699431ba
Elie Merhej <elie.merhej@pasqal.com>
2025-06-03 13:43:07 +0200

    Minimize energy of the pair state (#124)
    * writing the minimize pair method + tests
    
    * replace reshape with view
    
    * another one reshape to view
    
    * sticking to Hermitian matrices only and updating other parts
    
    * removing is_hermitian flag
    
    * quick refactor
    
    * Anton comment about tests

--- test/emu_mps/test_solver_utils.py
+++ test/emu_mps/test_solver_utils.py
@@ -1,24 +1,27 @@
 import torch
 from emu_base.math import krylov_exp
+from emu_mps.solver_utils import minimize_energy_pair
 from emu_mps import MPS, MPO, MPSConfig
-from emu_mps.tdvp import (
+from emu_mps.solver_utils import (
     apply_effective_Hamiltonian,
     right_baths,
     evolve_single,
     evolve_pair,
 )
 
+dtype = torch.complex128
+
 
 def test_right_baths_bell():
     # state = (|0> + |1>)^3 / norm
-    mps_factor1 = torch.tensor([[[1, 0], [0, 1]]], dtype=torch.complex128)
+    mps_factor1 = torch.tensor([[[1, 0], [0, 1]]], dtype=dtype)
     mps_factor2 = torch.tensor(
         [[[1, 0], [0, 0]], [[0, 0], [0, 1]]], dtype=torch.complex128
     )
-    mps_factor3 = torch.tensor([[[1], [0]], [[0], [1]]], dtype=torch.complex128)
+    mps_factor3 = torch.tensor([[[1], [0]], [[0], [1]]], dtype=dtype)
 
     # Hamiltonian X1*X2*X3
-    mpo_factor = torch.tensor([[[[0], [1]], [[1], [0]]]], dtype=torch.complex128)
+    mpo_factor = torch.tensor([[[[0], [1]], [[1], [0]]]], dtype=dtype)
 
     state = MPS([mps_factor1, mps_factor2, mps_factor3], eigenstates=("0", "1"))
     obs = MPO([mpo_factor] * 3)
@@ -32,27 +35,23 @@ def test_right_baths_bell():
         else:
             assert torch.allclose(
                 b,
-                torch.tensor(
-                    [[[0, 1]], [[1, 0]]], dtype=torch.complex128, device=b.device
-                ),
+                torch.tensor([[[0, 1]], [[1, 0]]], dtype=dtype, device=b.device),
             )
 
 
 def test_right_baths_total_magnetization():
     # Hamiltonian Z1+Z2+Z3
-    mpo_factor1 = torch.tensor(
-        [[[[1, 1], [0, 0]], [[0, 0], [-1, 1]]]], dtype=torch.complex128
-    )
+    mpo_factor1 = torch.tensor([[[[1, 1], [0, 0]], [[0, 0], [-1, 1]]]], dtype=dtype)
     mpo_factor2 = torch.tensor(
         [[[[1, 0], [0, 0]], [[0, 0], [1, 0]]], [[[1, 1], [0, 0]], [[0, 0], [-1, 1]]]],
         dtype=torch.complex128,
     )
     mpo_factor3 = torch.tensor(
-        [[[[1], [0]], [[0], [1]]], [[[1], [0]], [[0], [-1]]]], dtype=torch.complex128
+        [[[[1], [0]], [[0], [1]]], [[[1], [0]], [[0], [-1]]]], dtype=dtype
     )
 
     # state |111>
-    mps_factor = torch.tensor([[[0], [1]]], dtype=torch.complex128)
+    mps_factor = torch.tensor([[[0], [1]]], dtype=dtype)
 
     state = MPS([mps_factor] * 3, eigenstates=("0", "1"))
     obs = MPO([mpo_factor1, mpo_factor2, mpo_factor3])
@@ -60,24 +59,24 @@ def test_right_baths_total_magnetization():
     # The baths carry the information of the magnetization, so the baths have shape
     # (1,2,1), and L_i = [-i,1], which basically counts how magnetized the bath is.
     assert torch.allclose(
-        baths[0], torch.ones(1, 1, 1, dtype=torch.complex128, device=baths[0].device)
+        baths[0], torch.ones(1, 1, 1, dtype=dtype, device=baths[0].device)
     )
     assert torch.allclose(
         baths[1],
-        torch.tensor([[[1], [-1]]], dtype=torch.complex128, device=baths[1].device),
+        torch.tensor([[[1], [-1]]], dtype=dtype, device=baths[1].device),
     )
     assert torch.allclose(
         baths[2],
-        torch.tensor([[[1], [-2]]], dtype=torch.complex128, device=baths[2].device),
+        torch.tensor([[[1], [-2]]], dtype=dtype, device=baths[2].device),
     )
 
 
 def test_apply_2_site_effective_Hamiltonian():
-    left_bath = torch.randn(4, 3, 4, dtype=torch.complex128)
-    right_bath = torch.randn(7, 6, 7, dtype=torch.complex128)
-    state = torch.randn(4, 4, 7, dtype=torch.complex128)
-    left_ham = torch.randn(3, 2, 2, 8, dtype=torch.complex128)
-    right_ham = torch.randn(8, 2, 2, 6, dtype=torch.complex128)
+    left_bath = torch.randn(4, 3, 4, dtype=dtype)
+    right_bath = torch.randn(7, 6, 7, dtype=dtype)
+    state = torch.randn(4, 4, 7, dtype=dtype)
+    left_ham = torch.randn(3, 2, 2, 8, dtype=dtype)
+    right_ham = torch.randn(8, 2, 2, 6, dtype=dtype)
     ham = torch.einsum("ijkl,lmno->ijmkno", left_ham, right_ham).reshape(3, 4, 4, 6)
     actual = apply_effective_Hamiltonian(state, ham, left_bath, right_bath)
     assert actual.shape == (4, 4, 7)
@@ -95,10 +94,10 @@ def test_apply_2_site_effective_Hamiltonian():
 
 
 def test_apply_1_site_effective_Hamiltonian():
-    left_bath = torch.randn(4, 3, 4, dtype=torch.complex128)
-    right_bath = torch.randn(7, 6, 7, dtype=torch.complex128)
-    state = torch.randn(4, 2, 7, dtype=torch.complex128)
-    ham = torch.randn(3, 2, 2, 6, dtype=torch.complex128)
+    left_bath = torch.randn(4, 3, 4, dtype=dtype)
+    right_bath = torch.randn(7, 6, 7, dtype=dtype)
+    state = torch.randn(4, 2, 7, dtype=dtype)
+    ham = torch.randn(3, 2, 2, 6, dtype=dtype)
     actual = apply_effective_Hamiltonian(state, ham, left_bath, right_bath)
     assert actual.shape == (4, 2, 7)
     # this is the expression apply_2_site_Hamiltonian implements,
@@ -115,10 +114,10 @@ def test_apply_1_site_effective_Hamiltonian():
 
 def test_krylov_exp_krylov_norm_tolerance():
     # trivial bath
-    bath = torch.ones(1, 2, 1, dtype=torch.complex128)
+    bath = torch.ones(1, 2, 1, dtype=dtype)
 
-    left_ham = torch.zeros(2, 2, 2, 2, dtype=torch.complex128)
-    right_ham = torch.zeros(2, 2, 2, 2, dtype=torch.complex128)
+    left_ham = torch.zeros(2, 2, 2, 2, dtype=dtype)
+    right_ham = torch.zeros(2, 2, 2, 2, dtype=dtype)
     # X on first qubit
     left_ham[0, 0, 1, 0] = 1
     left_ham[0, 1, 0, 0] = 1
@@ -128,7 +127,7 @@ def test_krylov_exp_krylov_norm_tolerance():
     ham = torch.einsum("ijkl,lmno->ijmkno", left_ham, right_ham).reshape(2, 4, 4, 2)
 
     # the state |00>
-    state = torch.zeros(1, 4, 1, dtype=torch.complex128)
+    state = torch.zeros(1, 4, 1, dtype=dtype)
     state[0, 0, 0] = 1
 
     # i.e. u = X1
@@ -140,28 +139,28 @@ def test_krylov_exp_krylov_norm_tolerance():
     # b < norm_tolerance, since in the second iteration, b=0
     assert torch.allclose(
         result,
-        torch.tensor([[[0.0], [0.0], [1.0j], [0.0]]], dtype=torch.complex128),
+        torch.tensor([[[0.0], [0.0], [1.0j], [0.0]]], dtype=dtype),
     )
 
 
 def test_krylov_exp_krylov_exp_tolerance():
     # the bath will shift the bath index up one for each application of H_eff
     bath_size = 100
-    left_bath = torch.zeros(bath_size, 1, bath_size, dtype=torch.complex128)
+    left_bath = torch.zeros(bath_size, 1, bath_size, dtype=dtype)
     for i in range(bath_size - 1):
         left_bath[i + 1, 0, i] = 1.0
         left_bath[i, 0, i + 1] = 1.0
-    right_bath = torch.ones(1, 1, 1, dtype=torch.complex128)
+    right_bath = torch.ones(1, 1, 1, dtype=dtype)
     # all logic for this test is in the bath, but the code only works for qubits
     # so simply do the identity on the qubits
-    ham = torch.zeros(1, 2, 2, 1, dtype=torch.complex128)
+    ham = torch.zeros(1, 2, 2, 1, dtype=dtype)
     # identity on both qubit
     ham[0, 1, 1, 0] = 1
     ham[0, 0, 0, 0] = 1
     ham = torch.einsum("ijkl,lmno->ijmkno", ham, ham).reshape(1, 4, 4, 1)
 
     # the state 00 at the 0th bath index
-    state = torch.zeros(bath_size, 4, 1, dtype=torch.complex128)
+    state = torch.zeros(bath_size, 4, 1, dtype=dtype)
     state[0, 0, 0] = 1
 
     # The output state should have psi[m,:,m] = i^m/m!|00>
@@ -178,9 +177,7 @@ def test_krylov_exp_krylov_exp_tolerance():
     for i in range(14, 100):
         assert result[i, 0, 0] == 0.0 + 0.0j
     # also assert that we are proportional to the 00 state for each bath index
-    assert torch.allclose(
-        result[:, 1:, 0], torch.zeros(bath_size, 3, dtype=torch.complex128)
-    )
+    assert torch.allclose(result[:, 1:, 0], torch.zeros(bath_size, 3, dtype=dtype))
 
     # now test that the output of the algorithm approximates the exact result
     expected = torch.linalg.matrix_exp(1.0j * left_bath.reshape(bath_size, bath_size))[
@@ -190,10 +187,10 @@ def test_krylov_exp_krylov_exp_tolerance():
 
 
 def test_evolve_single():
-    left_bath = torch.rand(3, 4, 3, dtype=torch.complex128)
-    right_bath = torch.rand(4, 5, 4, dtype=torch.complex128)
-    state_factor = torch.rand(3, 2, 4, dtype=torch.complex128)
-    ham_factor = torch.rand(4, 2, 2, 5, dtype=torch.complex128)
+    left_bath = torch.rand(3, 4, 3, dtype=dtype)
+    right_bath = torch.rand(4, 5, 4, dtype=dtype)
+    state_factor = torch.rand(3, 2, 4, dtype=dtype)
+    ham_factor = torch.rand(4, 2, 2, 5, dtype=dtype)
 
     op = torch.einsum("abc,bdef,gfh->adgceh", left_bath, ham_factor, right_bath).reshape(
         3 * 2 * 4, -1
@@ -220,12 +217,12 @@ def test_evolve_single():
 
 
 def test_evolve_pair():
-    left_bath = torch.rand(3, 4, 3, dtype=torch.complex128)
-    right_bath = torch.rand(5, 6, 5, dtype=torch.complex128)
-    left_state_factor = torch.rand(3, 2, 4, dtype=torch.complex128)
-    right_state_factor = torch.rand(4, 2, 5, dtype=torch.complex128)
-    left_ham_factor = torch.rand(4, 2, 2, 5, dtype=torch.complex128)
-    right_ham_factor = torch.rand(5, 2, 2, 6, dtype=torch.complex128)
+    left_bath = torch.rand(3, 4, 3, dtype=dtype)
+    right_bath = torch.rand(5, 6, 5, dtype=dtype)
+    left_state_factor = torch.rand(3, 2, 4, dtype=dtype)
+    right_state_factor = torch.rand(4, 2, 5, dtype=dtype)
+    left_ham_factor = torch.rand(4, 2, 2, 5, dtype=dtype)
+    right_ham_factor = torch.rand(5, 2, 2, 6, dtype=dtype)
 
     op = torch.einsum(
         "abc,bdef,fghi,jik->adgjcehk",
@@ -260,3 +257,58 @@ def test_evolve_pair():
     actual = torch.tensordot(actual_left, actual_right, dims=1)
 
     assert torch.allclose(expected, actual, rtol=0, atol=1e-8)
+
+
+def test_minimize_energy_pair():
+    left_bath = torch.rand(2, 2, 2, dtype=dtype)
+    right_bath = torch.rand(2, 3, 2, dtype=dtype)
+    left_state_factor = torch.rand(2, 2, 4, dtype=dtype)
+    right_state_factor = torch.rand(4, 2, 2, dtype=dtype)
+    left_ham_factor = torch.rand(2, 2, 2, 5, dtype=dtype)
+    right_ham_factor = torch.rand(5, 2, 2, 3, dtype=dtype)
+
+    # Make the baths and ham factors hermitian tensors over the appropriate indices
+    left_bath = 0.5 * (left_bath + left_bath.permute(2, 1, 0).conj())
+    right_bath = 0.5 * (right_bath + right_bath.permute(2, 1, 0).conj())
+    left_ham_factor = 0.5 * (left_ham_factor + left_ham_factor.permute(0, 2, 1, 3).conj())
+    right_ham_factor = 0.5 * (
+        right_ham_factor + right_ham_factor.permute(0, 2, 1, 3).conj()
+    )
+
+    left_state_factor_shape = left_state_factor.shape
+    right_state_factor_shape = right_state_factor.shape
+
+    op = (
+        torch.einsum(
+            "abc,bdef,fghi,jik->adgjcehk",
+            left_bath,
+            left_ham_factor,
+            right_ham_factor,
+            right_bath,
+        )
+        .contiguous()
+        .view(2 * 2 * 2 * 2, -1)
+    )
+
+    eigen_energy, _ = torch.linalg.eigh(op)
+    expected_energy = eigen_energy[0].item()
+
+    actual_left, actual_right, actual_energy = minimize_energy_pair(
+        state_factors=[left_state_factor, right_state_factor],
+        baths=[left_bath, right_bath],
+        ham_factors=[left_ham_factor, right_ham_factor],
+        config=MPSConfig(max_bond_dim=12),
+        orth_center_right=False,
+        residual_tolerance=1e-8,
+    )
+
+    # check that resulting state tensors have correct shapes
+    assert actual_left.shape == left_state_factor_shape
+    assert actual_right.shape == right_state_factor_shape
+
+    # check: norm of new state ~ 1
+    actual_state = torch.tensordot(actual_left, actual_right, dims=1).view(-1)
+    assert torch.isclose(actual_state.norm(), torch.tensor(1.0, dtype=float), atol=1e-8)
+
+    # check the resulting energy
+    assert abs(actual_energy.real - expected_energy.real) <= 1e-8
