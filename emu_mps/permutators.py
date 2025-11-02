697fa96bbc98d3c770c21af2b1377cfec949ee5a
YPadawan <idrissalaouiamini@gmail.com>
2025-11-02 22:00:56 +0100

    added docstring and linting to the new permutators.py file. It was better to separate these functions from the mps_backend_impl.py, so that the latter could only contain necessary classes for the effective implementation of the MPS backend.

--- emu_mps/permutators.py
+++ emu_mps/permutators.py
@@ -1,9 +1,32 @@
+"""
+This module contains the different permute functions that are used in the context
+of the MPS backend.
+"""
+
+from collections import Counter
+
 import torch
 
+from pulser.backend import Results
+import emu_mps.optimatrix as optimat
 
+
+# pylint: disable=protected-access
+
+#TODO check if it is possible to avoid protected accesses from objects (e.g. results._results)
+
+#TODO correct docstrings
 
 
 def permute_bitstrings(results: Results, perm: torch.Tensor) -> None:
+    """
+    Bitstring permutator functions
+
+    Args:
+
+        results (Results): Result data
+        perm (torch.Tensor): Pytorch tensor that
+    """
     if "bitstrings" not in results.get_result_tags():
         return
     uuid_bs = results._find_uuid("bitstrings")
@@ -15,6 +38,13 @@ def permute_bitstrings(results: Results, perm: torch.Tensor) -> None:
 
 
 def permute_occupations_and_correlations(results: Results, perm: torch.Tensor) -> None:
+    """
+    Function permuting the occupations and correlations
+
+    Args:
+        results (Results): Results data structure
+        perm (torch.Tensor): Pytorch permutation matrix
+    """
     for corr in ["occupation", "correlation_matrix"]:
         if corr not in results.get_result_tags():
             continue
@@ -32,6 +62,14 @@ def permute_occupations_and_correlations(results: Results, perm: torch.Tensor) -
 
 
 def permute_atom_order(results: Results, perm: torch.Tensor) -> None:
+    """
+    Function permuting the atom order
+
+    Args:
+
+        results (Results): Results data structure
+        perm (torch.Tensor): Pytorch permutation matrix
+    """
     at_ord = list(results.atom_order)
     at_ord = optimat.permute_list(at_ord, perm)
     results.atom_order = tuple(at_ord)
