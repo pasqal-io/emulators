a23f4acf0b128db2b58379a1b0238bc107c6497a
Elie <elie.merhej@pasqal.com>
2025-12-02 16:01:25 +0100

    fixing pulser adapter bug

--- emu_base/pulser_adapter.py
+++ emu_base/pulser_adapter.py
@@ -101,31 +101,47 @@ def _extract_omega_delta_phi(
             t1 = math.floor(t)
             t2 = math.ceil(t)
             for q_pos, q_id in enumerate(qubit_ids):
-                omega[i, q_pos] = (
-                    locals_a_d_p[q_id]["amp"][t1] + locals_a_d_p[q_id]["amp"][t2]
-                ) / 2.0
-                delta[i, q_pos] = (
-                    locals_a_d_p[q_id]["det"][t1] + locals_a_d_p[q_id]["det"][t2]
-                ) / 2.0
-                phi[i, q_pos] = (
-                    locals_a_d_p[q_id]["phase"][t1] + locals_a_d_p[q_id]["phase"][t2]
-                ) / 2.0
+                if q_id in locals_a_d_p:
+                    omega[i, q_pos] = (
+                        locals_a_d_p[q_id]["amp"][t1] + locals_a_d_p[q_id]["amp"][t2]
+                    ) / 2.0
+                    delta[i, q_pos] = (
+                        locals_a_d_p[q_id]["det"][t1] + locals_a_d_p[q_id]["det"][t2]
+                    ) / 2.0
+                    phi[i, q_pos] = (
+                        locals_a_d_p[q_id]["phase"][t1] + locals_a_d_p[q_id]["phase"][t2]
+                    ) / 2.0
+                else:
+                    omega[i, q_pos] = 0.0
+                    delta[i, q_pos] = 0.0
+                    phi[i, q_pos] = 0.0
+
         else:
             # We're in the final step and dt=1, approximate this using linear extrapolation
             # we can reuse omega_1 and omega_2 from before
             for q_pos, q_id in enumerate(qubit_ids):
-                delta[i, q_pos] = (
-                    3.0 * locals_a_d_p[q_id]["det"][t2] - locals_a_d_p[q_id]["det"][t1]
-                ) / 2.0
-                phi[i, q_pos] = (
-                    3.0 * locals_a_d_p[q_id]["phase"][t2]
-                    - locals_a_d_p[q_id]["phase"][t1]
-                ) / 2.0
-                omega[i, q_pos] = max(
-                    (3.0 * locals_a_d_p[q_id]["amp"][t2] - locals_a_d_p[q_id]["amp"][t1])
-                    / 2.0,
-                    0.0,
-                )
+                if q_id in locals_a_d_p:
+                    delta[i, q_pos] = (
+                        3.0 * locals_a_d_p[q_id]["det"][t2]
+                        - locals_a_d_p[q_id]["det"][t1]
+                    ) / 2.0
+                    phi[i, q_pos] = (
+                        3.0 * locals_a_d_p[q_id]["phase"][t2]
+                        - locals_a_d_p[q_id]["phase"][t1]
+                    ) / 2.0
+                    omega[i, q_pos] = max(
+                        (
+                            3.0 * locals_a_d_p[q_id]["amp"][t2]
+                            - locals_a_d_p[q_id]["amp"][t1]
+                        )
+                        / 2.0,
+                        0.0,
+                    )
+                else:
+                    omega[i, q_pos] = 0.0
+                    delta[i, q_pos] = 0.0
+                    phi[i, q_pos] = 0.0
+
     return omega, delta, phi
 
 
