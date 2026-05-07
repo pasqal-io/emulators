9e6551f4c92ddde1cdf606e629f1b1852d36057b
Anton Quelle <a.quelle@pasqal.com>
2026-05-07 09:59:42 +0200

    torch version cap

--- test_dependency_versions.py
+++ test_dependency_versions.py
@@ -8,11 +8,24 @@ from typing import Optional
 file_dir = Path(__file__).parent
 
 
-def extract_version_from_file(filepath: Path, pattern: str) -> Optional[str]:
+def extract_version_from_pycff_file(filepath: Path) -> Optional[str]:
     with open(filepath, "r") as f:
         for line in f:
             match = re.search(
-                pattern + r'\s*>?=*\s*"?([0-9]+(?:\.[^\s"\.\,]+){2})"?', line
+                r'^_*version[_,\s]*[:,=]\s*"?([0-9]+(?:\.[^\s"\.\,]+){2})"?', line
+            )
+            if match:
+                return match.group(1)
+    raise Exception("pattern not found")
+
+
+def extract_version_from_oml_file(filepath: Path, pattern: str) -> Optional[str]:
+    with open(filepath, "r") as f:
+        for line in f:
+            match = re.search(
+                pattern
+                + r'\s*>?=*\s*"?([0-9]+(?:\.[^\s"\.\,]+){2}(,<[0-9]+(?:\.[^\s"\.\,]+){2})?)"?',
+                line,
             )
             if match:
                 return match.group(1)
@@ -27,28 +40,30 @@ def fail(msg: str) -> None:
 print("Checking emu_base for emu-sv and emu-mps:")
 
 # emu_sv -> emu_base
-sv_dep = extract_version_from_file(file_dir / "ci/emu_sv/pyproject.toml", "emu-base")
+sv_dep = extract_version_from_oml_file(file_dir / "ci/emu_sv/pyproject.toml", "emu-base")
 print(f" - emu-sv depends on emu-base version {sv_dep}")
 
 # emu_mps -> emu_base
-mps_dep = extract_version_from_file(file_dir / "ci/emu_mps/pyproject.toml", "emu-base")
+mps_dep = extract_version_from_oml_file(
+    file_dir / "ci/emu_mps/pyproject.toml", "emu-base"
+)
 print(f" - emu-mps depends on emu-base version {mps_dep}")
 
 # emu_base version
-base_version = extract_version_from_file(file_dir / "emu_base/__init__.py", "__version__")
+base_version = extract_version_from_pycff_file(file_dir / "emu_base/__init__.py")
 print(f" - emu-base is version {base_version}")
 
 # emu_sv version
-sv_version = extract_version_from_file(file_dir / "emu_sv/__init__.py", "__version__")
+sv_version = extract_version_from_pycff_file(file_dir / "emu_sv/__init__.py")
 print(f" - emu-sv is version {sv_version}")
 
 # emu_mps version
-mps_version = extract_version_from_file(file_dir / "emu_mps/__init__.py", "__version__")
+mps_version = extract_version_from_pycff_file(file_dir / "emu_mps/__init__.py")
 print(f" - emu-mps is version {mps_version}")
 
 # citation version
 
-citation_version = extract_version_from_file(file_dir / "CITATION.cff", "^version:")
+citation_version = extract_version_from_pycff_file(file_dir / "CITATION.cff")
 print(f" - citation version is {citation_version}")
 
 if (
@@ -73,13 +88,13 @@ if sv_dep != base_version:
 print("Checking pulser_core version:")
 
 # pulser_core -> emu_base
-pulser_base_dep = extract_version_from_file(
+pulser_base_dep = extract_version_from_oml_file(
     file_dir / "ci/emu_base/pyproject.toml", r"pulser-core\[torch\]"
 )
 print(f" - emu-base depends on pulser-core version {pulser_base_dep}")
 
 # pulser_core in root
-pulser_root_dep = extract_version_from_file(
+pulser_root_dep = extract_version_from_oml_file(
     file_dir / "pyproject.toml", r"pulser-core\[torch\]"
 )
 print(f" - The root package depends on pulser-core version {pulser_root_dep}")
@@ -88,7 +103,7 @@ if pulser_root_dep != pulser_base_dep:
     fail(f"{pulser_root_dep} != {pulser_base_dep}")
 
 # pulser_core in .pre-commit-config.yaml
-pulser_pre_commit_version = extract_version_from_file(
+pulser_pre_commit_version = extract_version_from_oml_file(
     file_dir / ".pre-commit-config.yaml", r"pulser-core\[torch\]"
 )
 print(f" - pre-commit uses pulser_core version {pulser_pre_commit_version}")
@@ -102,18 +117,31 @@ if pulser_pre_commit_version != pulser_root_dep:
 print("Checking torch:")
 
 # torch in .pre-commit-config.yaml
-torch_pre_commit_version = extract_version_from_file(
+torch_pre_commit_version = extract_version_from_oml_file(
     file_dir / ".pre-commit-config.yaml", "torch"
 )
 print(f" - pre-commit uses torch version {torch_pre_commit_version}")
 
 # torch in pyproject.toml
-torch_pyproject_version = extract_version_from_file(file_dir / "pyproject.toml", "torch")
+torch_pyproject_version = extract_version_from_oml_file(
+    file_dir / "pyproject.toml", "torch"
+)
 print(f" - pyproject.toml uses torch version {torch_pyproject_version}")
 
 if torch_pre_commit_version != torch_pyproject_version:
     fail(
         f" - torch in .pre-commit version {torch_pre_commit_version}"
+        f" != torch in pyproject.toml {torch_pyproject_version}"
+    )
+
+torch_emu_base_version = extract_version_from_oml_file(
+    file_dir / "ci/emu_base/pyproject.toml", "torch"
+)
+print(f" - emu-base uses torch version {torch_pyproject_version}")
+
+if torch_emu_base_version != torch_pyproject_version:
+    fail(
+        f" - torch in emu-base version {torch_pre_commit_version}"
         f" != torch in pyproject.toml {torch_pyproject_version}"
     )
 
